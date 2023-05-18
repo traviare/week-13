c315462075db374b709e8f545752c48660467ae5
traviare <madyka.violetta@gmail.com>
2023-05-18 18:58:27 +0300

    add homework week-13

---
+++ index.js
@@ -0,0 +1,59 @@
+// задание № 1
+
+let salaryCleaner = 40000
+let salarySinger = 22000
+console.log (salaryCleaner + salarySinger);
+
+// задание № 2
+
+let word1 = "Владивосток"
+let number1 = 2000
+console.log ( `${word1} ${number1}` );
+
+// задание № 3
+
+let takeMessage = 128
+let likePhoto = 254
+let readeVkGroupe = 137
+let watchYouTube = 201
+console.log ((takeMessage + likePhoto + readeVkGroupe + watchYouTube) / 60);
+
+// задание № 4
+
+console.log (2**5);
+
+// задание № 5
+
+console.log ("Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!")
+
+// задание № 6
+
+const greetings = "Привет";
+const name1 = "Кот"; // заменила name на name1 т.к начал ругаться что это имя устарело
+console.log ( `${greetings}, ${name1}` );
+
+// задание № 7
+
+let time;
+time = 34;
+console.log (time);
+console.log ('Старт поездки. Осталось минут: ' + time);
+console.log (time - 15);
+time = 19
+console.log ('Немного сториз в соцсетях. Осталось минут: ' + time);
+console.log (time - 10);
+time = 9
+console.log ('Немного не новостей, но событий. Осталось минут: ' + time);
+time = 0
+console.log ("Вы приехали. Добро пожаловать в Москву");
+
+// задание № 8
+
+let fahrenheit = 451;
+let celsius;
+console.log ((fahrenheit - 32) / 1.8);
+celsius = 232.77;
+console.log (`${fahrenheit} градуса по Фаренгейту - это ${celsius} градуса по Цельсию.`);
+
+
+
